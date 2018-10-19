9157016d334bbb853271f2f05768637674b0947b
noahschnitzer <nschnitz@umich.edu>
2018-10-19 19:14:55 -0400

    using new pointer

--- vis4D.m
+++ vis4D.m
@@ -1,38 +1,31 @@
 function vis4D(im_4d)
-   % load im_4d_3PLD.mat
-    im_ave = squeeze( mean( mean( im_4d, 1), 2) );
-    im_ave_log = log(im_ave + 1);
-    
-    [nA1,nA2,ny,nx] = size(im_4d);
-    
-    A1_inds = 1:nA1;
-    A2_inds = 1:nA2;
-    
 
+    im_ave = squeeze( mean( mean( im_4d, 1), 2) );
+    
     f = figure;
     ax1 = subplot(1,2,1);
     ax2 = subplot(1,2,2);
     
-    
-    imagesc(ax1,im_ave_log)
-    imagesc(ax2,zeros(ny,nx))
-    %colormap(f,'inferno')
-    
+    imagesc(ax1,im_ave)
+    colormap(f,'inferno')
     axis(ax1,'equal','off')
-    h1 = impoint(ax1,[10,10]);
-    draw34D([10,10],im_4d,ax2);
+    h1 = drawpoint(ax1,'Deletable',false,'Position',[50 50]);
+    im2 = imagesc(ax2,squeeze(im_4d(:,:,50, 50)));    
+    axis(ax2,'equal','tight','off')
+    colorbar(ax2)
+
     
-    h1.addNewPositionCallback(@(p) draw34D(p,im_4d,ax2));
-      
+   addlistener(h1,'MovingROI',@(src,evnt) draw4D(evnt,im_4d,im2,ax2));
 
 end
 
-function draw34D(xy,im_4d,ax)
-    xy = round(xy);
+function draw4D(evenData,im_4d,im,ax)
+    
+    xy = round(evenData.CurrentPosition);
     x = xy(1); y = xy(2);
-     
     sup_im = squeeze(im_4d(:,:,y,x));
-    imagesc(ax,sup_im)
-    axis(ax,'equal','tight')
+    set(im,'CData',sup_im)
     colorbar(ax)
+
 end
+
