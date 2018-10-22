2f131330afb89462fb11b3df6a816735124b595f
Suk Hyun Sung <sukhsung@umich.edu>
2018-10-21 22:48:32 -0400

    renamed myImagesc to imageBC

--- imageBC.m
+++ imageBC.m
@@ -1,16 +1,23 @@
-function [f,h] = myImagesc(im)
+function [f,h] = imageBC(im)
+    % Brightness & Contrast enabled imagesc
+    % by Suk Hyun Sung @ hovdenlab
+    % sukhsung@umich.edu
     
-    cmin = min(im(:));
-    cmax = max(im(:));
-    
+    % Create new figure and perform imagesc,
+    % return figure, imagesc handle
     
     f = figure;
     h = imagesc(im);
 
+    % Find default min, max
+    cmin = min(im(:));
+    cmax = max(im(:)); 
+    
+    % Add sliders
     sl1 = uicontrol('style','slider','position',[10 60 20 300],'min', cmin, 'max', cmax,'Value', cmin);
     sl2 = uicontrol('style','slider','position',[35 60 20 300],'min', cmin, 'max', cmax,'Value', cmax);
     
+    % Listen to slider values and change B & C
     addlistener(sl1, 'Value', 'PostSet',@(hObject,eventdata) caxis([get(sl1,'Value'), get(sl2,'Value')]));
     addlistener(sl2, 'Value', 'PostSet',@(hObject,eventdata) caxis([get(sl1,'Value'), get(sl2,'Value')]));
-    
 end
