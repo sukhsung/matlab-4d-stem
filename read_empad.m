cfa8fc77f507ddc0f8da21f27d913c6650ae9d48
jtschwar <jonathanschwartz@0587458745.wireless.umich.net>
2022-12-15 15:45:04 -0500

    allow user input for empad version (default 1.5)

--- read_empad.m
+++ read_empad.m
@@ -2,7 +2,7 @@
 % takes in file name, scan dimension
 % returns empad_data, 4D matrix with dimensions: 
 %   [detectorx, detectory,scanx, scany]
-function [empad_data] = read_empad(fname, ns)
+function [empad_data] = read_empad(fname, ns, empad_old)
     fid = fopen( fname );
 
     nsx = ns;
@@ -11,13 +11,15 @@ function [empad_data] = read_empad(fname, ns)
     ny = 128;
 
 
-    A = fread(fid, nx*(ny)*nsx*nsy,'long',0,'l');
-    A = reshape(A,[ny, nx,nsx,nsy]);
-
-    empad_data = A(:,1:end,:,:);
-
-%     A = fread(fid, nx*(ny+2)*nsx*nsy,'long',0,'l');
-%     A = reshape(A,[ny, nx+2,nsx,nsy]);
-% 
-%     empad_data = A(:,1:end-2,:,:);
+    if nargin == 2 || empad_old == false
+        A = fread(fid, nx*(ny+2)*nsx*nsy,'long',0,'l');
+        A = reshape(A,[ny, nx+2,nsx,nsy]);
+    
+        empad_data = A(:,1:end-2,:,:);
+    else
+        A = fread(fid, nx*(ny)*nsx*nsy,'long',0,'l');
+        A = reshape(A,[ny, nx,nsx,nsy]);
+    
+        empad_data = A(:,1:end,:,:);
+    end
 end
